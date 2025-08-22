9ca3c3770224627015abba1dc8bc7acfb97f4b07
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-08-22 10:44:38 -0300

    first commit

---
+++ src/main/kotlin/com/ricky/meus_gastos/MeusGastosApplication.kt
@@ -0,0 +1,12 @@
+package com.ricky.meus_gastos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class
+
+
+fun main(args: Array<String>) {
+	runApplication<MeusGastosApplication>(*args)
+}
