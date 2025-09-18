208c38b0adaf99a8f72f7c091fe8a86b1a2bbdbe
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-09-18 11:30:54 -0300

    ajustes transaction data e handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,14 +20,15 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/categories", app.requireActivatedUser(app.listCategoriesHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/categories", app.requireActivatedUser(app.createCategoryHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/categories/:id", app.requireActivatedUser(app.showCategoryHandler))
-	router.HandlerFunc(http.MethodPut, "/v1/categories:id", app.requireActivatedUser(app.updateCategoryHandler))
+	router.HandlerFunc(http.MethodPut, "/v1/categories/:id", app.requireActivatedUser(app.updateCategoryHandler))
 	router.HandlerFunc(http.MethodDelete, "/v1/categories/:id", app.requireActivatedUser(app.deleteCategoryHandler))
 
 	router.HandlerFunc(http.MethodGet, "/v1/transactions", app.requireActivatedUser(app.listTransactionsHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/transactions", app.requireActivatedUser(app.createTransactionHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/transactions/:id", app.requireActivatedUser(app.showTransactionHandler))
-	router.HandlerFunc(http.MethodPut, "/v1/transactions/:id", app.requireActivatedUser(app.updateTransactionHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/transactions/:id", app.requireActivatedUser(app.deleteTransactionHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/transactions/find/:id", app.requireActivatedUser(app.showTransactionHandler))
+	router.HandlerFunc(http.MethodPut, "/v1/transactions/update/:id", app.requireActivatedUser(app.updateTransactionHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/transactions/delete/:id", app.requireActivatedUser(app.deleteTransactionHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/transactions/category/:id", app.requireActivatedUser(app.listTransactionsByCategoryIDHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
--- cmd/api/transaction.go
+++ cmd/api/transaction.go
@@ -9,59 +9,6 @@ import (
 	"time"
 )
 
-func (app *application) listTransactionsByCategoryTypeHandler(w http.ResponseWriter, r *http.Request) {
-	var input struct {
-		Name         string
-		CategoryType data.TypeCategoria
-		StartDate    time.Time
-		EndDate      time.Time
-		data.Filters
-	}
-
-	v := validator.New()
-
-	qs := r.URL.Query()
-	input.Name = app.readString(qs, "description", "")
-	categoryStr := app.readString(qs, "category_type", "")
-	if categoryStr != "" {
-		input.CategoryType = data.TypeCategoriaFromString(categoryStr)
-	}
-	input.StartDate = *app.readDate(qs, "start", "2006-01-02")
-	input.EndDate = *app.readDate(qs, "end", "2006-01-02")
-	input.Filters.Page = app.readInt(qs, "page", 1, v)
-	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
-	input.Filters.Sort = app.readString(qs, "sort", "id")
-	input.Filters.SortSafelist = []string{"id", "description", "-id", "-description"}
-
-	if data.ValidateFilters(v, input.Filters); !v.Valid() {
-		app.failedValidationResponse(w, r, v.Errors)
-		return
-	}
-
-	user := app.contextGetUser(r)
-	transactions, metadata, err := app.models.Transactions.GetAllByCategoryTypeAndUser(input.Name, user.ID, input.StartDate, input.EndDate, input.CategoryType, input.Filters)
-
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-
-	transactionsDTO := []*data.TransactionDTO{}
-	for _, t := range transactions {
-		err = prepareTransactionForResponse(app, t, user)
-		if err != nil {
-			app.serverErrorResponse(w, r, err)
-			return
-		}
-		transactionsDTO = append(transactionsDTO, t.ToDTO())
-	}
-
-	err = app.writeJSON(w, http.StatusOK, envelope{"transactions": transactionsDTO, "metadata": metadata}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-	}
-}
-
 func (app *application) listTransactionsByCategoryIDHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
@@ -121,11 +68,20 @@ func (app *application) listTransactionsHandler(w http.ResponseWriter, r *http.R
 	var input struct {
 		Name string
 		data.Filters
+		CategoryType data.TypeCategoria
+		StartDate    time.Time
+		EndDate      time.Time
 	}
 
 	v := validator.New()
 
 	qs := r.URL.Query()
+	categoryStr := app.readString(qs, "category_type", "")
+	if categoryStr != "" {
+		input.CategoryType = data.TypeCategoriaFromString(categoryStr)
+	}
+	input.StartDate = *app.readDate(qs, "start", "2006-01-02")
+	input.EndDate = *app.readDate(qs, "end", "2006-01-02")
 	input.Name = app.readString(qs, "description", "")
 	input.Filters.Page = app.readInt(qs, "page", 1, v)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
@@ -138,7 +94,13 @@ func (app *application) listTransactionsHandler(w http.ResponseWriter, r *http.R
 	}
 
 	user := app.contextGetUser(r)
-	transactions, metadata, err := app.models.Transactions.GetAllByUser(input.Name, user.ID, input.Filters)
+	transactions, metadata, err := app.models.Transactions.GetAllByUser(
+		input.Name, user.ID,
+		input.StartDate,
+		input.EndDate,
+		input.CategoryType,
+		input.Filters,
+	)
 
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
